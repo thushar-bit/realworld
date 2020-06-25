10c55c0d96f49ecced7ad1885f753e619f293e1d
thushar <55412054+thushar-bit@users.noreply.github.com>
2020-06-25 15:49:09 +0530

    Update hmmmmm.py

--- hmmmmm.py
+++ hmmmmm.py
@@ -1,6 +1,8 @@
-km = eval(input('enter the km'))
-litres = eval(input('enter the litres'))
+miles = eval(input('enter the km'))
+gallons = eval(input('enter the litres'))
 mpg = miles / gallon
 litres = gallon * 3.78
 km = miles * 1.609
+fuel_effi = (litres / km) * 100
 print(mpg)
+print(fuel_effi)
