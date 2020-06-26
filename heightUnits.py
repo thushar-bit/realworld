083c9c31192d4a95721c752a7b8c484816741c1e
thushar <55412054+thushar-bit@users.noreply.github.com>
2020-06-26 11:52:17 +0530

    Add files via upload

---
+++ heightUnits.py
@@ -0,0 +1,7 @@
+feet = eval(input('enter the number in feet '))
+inches = eval(input('enter the number of inches'))
+feeti = feet * 12
+feetc = feeti * 2.54
+centi = inches * 2.54
+total = feetc + centi
+print(total)
