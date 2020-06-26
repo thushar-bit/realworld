4adc56b8f097932380a2495c9be51811510a9cd5
thushar <55412054+thushar-bit@users.noreply.github.com>
2020-06-26 11:58:16 +0530

    Update heightUnits.py

--- heightUnits.py
+++ heightUnits.py
@@ -2,6 +2,8 @@ feet = eval(input('enter the number in feet '))
 inches = eval(input('enter the number of inches'))
 feeti = feet * 12
 feetc = feeti * 2.54
+yards = feet * 0.333333
+miles = feet * 0.000189394
 centi = inches * 2.54
 total = feetc + centi
-print(total)
+print(total, yard, miles)
