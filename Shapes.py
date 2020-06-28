6f0ff6645022c19f7c4e8f26dc916c1ceb58fc4f
thushar <55412054+thushar-bit@users.noreply.github.com>
2020-06-28 11:45:47 +0530

    just describes shapes

---
+++ Shapes.py
@@ -0,0 +1,20 @@
+x = int(input(''))
+if x >= 3 and x <= 10:
+    if x == 3:
+        print('its a triangle')
+    elif x == 4:
+        print('its is square')
+    elif x == 5:
+        print('its a pentagon')
+    elif x == 6:
+        print('its a hexagon')
+    elif x == 7:
+        print('its a heptagon')
+    elif x == 8:
+        print('its a octagon')
+    elif x == 9:
+        print('its a nonagon')
+    elif x == 10:
+        print('its a decagon')
+else:
+    print('it is not a shape')
